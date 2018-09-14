f036a775c89ae29353586dc5c613bc78fc6c6d10
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-09-14 13:47:33 -0400

    Added skeleton puzzlesover file.

---
+++ Project1/puzzlesolver.py
@@ -0,0 +1,25 @@
+#################################################################
+#
+# Alessio Mazzone
+# ALM388@pitt.edu
+#
+#
+# CS 1571 Aritificial Intelligence
+#
+#
+# Project 1 
+#
+#################################################################
+
+
+import sys
+
+
+def main(argv):
+    
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
